ab66f191c78df21a6d94569e5b02ff6a68b8a8ac
Jesús Burgos <jesus@visrez.com>
2015-11-02 16:54:12 +0100

    Stubs an implementation of Comparable on Card.

--- lib/card.rb
+++ lib/card.rb
@@ -1,13 +1,30 @@
 class Card
+  include Comparable
+
   VALID_RANKS = %w(2 3 4 5 6 7 8 9 T J Q K A)
   VALID_SUITS = %w(H D C S)
+
+  attr_reader :rank, :suit
+
   def initialize(card_identifier)
     @rank, @suit = card_identifier.chars
 
     validate!
   end
 
-private
+  def <=>(other)
+    if self.suit == other.suit
+      self.rank_as_integer <=> other.rank_as_integer
+    else
+      nil
+    end
+  end
+
+protected
+
+  def rank_as_integer
+    VALID_RANKS.index @rank
+  end
 
   def validate!
     raise ArgumentError, "Rank '#{@rank}' is invalid" unless rank_is_valid?
--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -30,4 +30,64 @@ describe Card do
       end
     end
   end
+
+  describe "#<=>" do
+    it "will return -1 if the receiver card is less than the other card" do
+      card_pairs = [
+        %w(7D AD),
+        %w(JD AD),
+        %w(KD AD),
+        %w(TD QD)
+      ]
+
+      card_pairs.each do |card, greater_card|
+        card = Card.new(card)
+        greater_card = Card.new(greater_card)
+
+        expect(card <=> greater_card).to eq(-1)
+      end
+    end
+
+    it "will return 1 if the receiver card is greater than the other card" do
+      card_pairs = [
+        %w(7D 6D),
+        %w(JD 6D),
+        %w(KD JD),
+        %w(TD 2D)
+      ]
+
+      card_pairs.each do |card, lesser_card|
+        card = Card.new(card)
+        lesser_card = Card.new(lesser_card)
+
+        expect(card <=> lesser_card).to eq(1)
+      end
+    end
+
+    it "will return 0 if both cards have the same rank and suit" do
+      card_a = Card.new("8D")
+      card_b = Card.new("8D")
+
+      expect(card_a <=> card_b).to eq(0)
+    end
+  end
+
+  describe "#next" do
+    it "will return the next card sorted by rank" do
+      card_sequences = [
+        %w(2S 3S),
+        %w(KH AH),
+        %w(TH JH),
+        %w(9H TH)
+      ]
+
+      card_sequences.each do |card, next_card|
+        expect(Card.new(card).next).to eq(Card.new(next_card))
+      end
+    end
+  end
+
+  describe "#prev" do
+
+  end
 end
