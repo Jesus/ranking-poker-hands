440e9c4e1ce7b74d164bbf892015a0db372efb8f
Jesús Burgos <jesus@visrez.com>
2015-11-03 20:44:37 +0100

    Adds Flush#cards_match?

--- lib/poker_hands/flush.rb
+++ lib/poker_hands/flush.rb
@@ -1,4 +1,7 @@
 require 'poker_hand'
 
 class Flush < PokerHand
+  def cards_match?
+    is_flush?
+  end
 end
---
+++ spec/poker_hands/flush_spec.rb
@@ -0,0 +1,37 @@
+require "spec_helper"
+require "poker_hands/flush"
+
+describe Flush do
+  describe "#cards_match?" do
+    it "is true if the given hand has full house" do
+      poker_hands = [
+        %w(5H 5H 5H 3H 3H),
+        %w(6S 5S 5S 6S 6S),
+        %w(AH AH JH JH AH),
+        %w(2S 2S AS AS 2S)
+      ]
+
+      poker_hands.each do |poker_hand|
+        poker_hand = described_class.new(poker_hand)
+
+        expect(poker_hand.cards_match?).to be_truthy
+      end
+    end
+
+    it "is false if the given hand doesn't have full house" do
+      poker_hands = [
+        %w(5H 5H 7H 7H 9D),
+        %w(6H 2H 3D 4H 3H),
+        %w(TS 9H 8H 7H QH),
+        %w(5S 6S 7S 9C 9S),
+        %w(8S KS JS QH TS)
+      ]
+
+      poker_hands.each do |poker_hand|
+        poker_hand = described_class.new(poker_hand)
+
+        expect(poker_hand.cards_match?).to be_falsey
+      end
+    end
+  end
+end
