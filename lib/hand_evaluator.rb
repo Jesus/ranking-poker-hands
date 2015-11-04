393834e0efe18dd19c8f488beedc35f3a2859b36
Jesús Burgos <jesus@visrez.com>
2015-11-04 02:49:22 +0100

    Implements #return_stronger_hand

--- lib/hand_evaluator.rb
+++ lib/hand_evaluator.rb
@@ -1,5 +1,14 @@
 class HandEvaluator
   def return_stronger_hand(left, right)
-    
+    left_hand = PokerHand.new(left).casted
+    right_hand = PokerHand.new(right).casted
+
+    if left_hand > right_hand
+      return left
+    elsif right_hand > left_hand
+      return right
+    else
+      raise "now what?"
+    end
   end
 end
