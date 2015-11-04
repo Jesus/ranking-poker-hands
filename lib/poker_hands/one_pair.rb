d0228b3dba11331d14c53d5534c4421a266c93fd
Jesús Burgos <jesus@visrez.com>
2015-11-04 02:21:46 +0100

    Adds comparer for pairs.

--- lib/poker_hands/one_pair.rb
+++ lib/poker_hands/one_pair.rb
@@ -4,4 +4,35 @@ class OnePair < PokerHand
       rank_count(rank) == 2
     end
   end
+
+protected
+
+  def comparer_methods
+    super + [
+      :pair_rank,
+      :first_side_card_rank,
+      :second_side_card_rank,
+      :third_side_card_rank
+    ]
+  end
+
+  def pair_rank
+    x_of_a_kind_rank(2)
+  end
+
+  def first_side_card_rank
+    sorted_side_cards[2]
+  end
+
+  def second_side_card_rank
+    sorted_side_cards[1]
+  end
+
+  def third_side_card_rank
+    sorted_side_cards[0]
+  end
+
+  def sorted_side_cards
+    @hand.cards.select { |card| card.rank != pair_rank }.sort
+  end
 end
--- spec/poker_hands/one_pair_spec.rb
+++ spec/poker_hands/one_pair_spec.rb
@@ -2,6 +2,57 @@ require "spec_helper"
 require "poker_hands"
 
 describe OnePair do
+  describe "#<=>" do
+    it "returns -1 if the receiver is smaller" do
+      receiver = described_class.new(%w(TH TH 6D 3S KH))
+      other    = described_class.new(%w(KH KH 2H 3H TH))
+
+      expect(receiver<=>(other)).to eq(-1)
+    end
+
+    it "returns -1 if the receiver is smaller (with tie)" do
+      receiver = described_class.new(%w(KH KH 9H 9C 4S))
+      other    = described_class.new(%w(KH KH TS AH 3H))
+
+      expect(receiver<=>(other)).to eq(-1)
+    end
+
+    it "returns -1 if the receiver is smaller (with two ties)" do
+      receiver = described_class.new(%w(KH KH AH 2C 4S))
+      other    = described_class.new(%w(KH KH AH QH 5H))
+
+      expect(receiver<=>(other)).to eq(-1)
+    end
+
+    it "returns -1 if the receiver is smaller (with three ties)" do
+      receiver = described_class.new(%w(KH KH 3H AC TS))
+      other    = described_class.new(%w(KH KH 4H AH TH))
+
+      expect(receiver<=>(other)).to eq(-1)
+    end
+
+    it "returns 1 if there receiver is greater" do
+      receiver = described_class.new(%w(TH 3H 3H AH 4H))
+      other    = described_class.new(%w(7H 3H AH 2S 2H))
+
+      expect(receiver<=>(other)).to eq(1)
+    end
+
+    it "returns 1 if there receiver is greater (with tie)" do
+      receiver = described_class.new(%w(TH AH 2H 9H 9H))
+      other    = described_class.new(%w(TH QH 9H 9H 3C))
+
+      expect(receiver<=>(other)).to eq(1)
+    end
+
+    it "returns 0 if there's a draw" do
+      receiver = described_class.new(%w(TH TD 2H 9H 9H))
+      other    = described_class.new(%w(TH TD 2H 9H 9H))
+
+      expect(receiver<=>(other)).to eq(0)
+    end
+  end
+
   describe "#cards_match?" do
     it "is true if the given hand has a pair" do
       poker_hands = [
